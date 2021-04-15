110a89152a2ad0ef9826820bc8637ff7341583a0
Rina226 <1977notw.q@gmail.com>
2021-04-15 13:41:46 +0900

    0322_WD_C04

---
+++ script/script.js
@@ -0,0 +1,37 @@
+$(function() {
+
+    /* GNB */
+    $("#gnb li").mouseover(function(){
+        $(this).children("ul.subMenu").stop().fadeIn(1000);
+    });
+    $("#gnb li").mouseout(function(){
+        $(this).children("ul.subMenu").stop().fadeOut(800);
+    });
+    /* GNB */
+
+
+    /* slideshow */
+    setInterval(fnSlide, 3000);
+    function fnSlide() {
+        $("#slide a:first-child").fadeOut(600);
+        $("#slide a:nth-child(2)").fadeIn(
+            1000,
+            function() {
+                $("#slide a:first-child").insertAfter("#slide a:last-child");
+            }
+        );
+    }
+
+    /* slideshow */
+
+    /*modalLayerPopup*/
+    $(".layerPopupPoint").click(function(){
+      $("#layerBG").css({"display": "block"});
+    });
+
+    $("#layerBG #closeBtn").click(function(){
+        $("#layerBG").css({"display": "none"});
+    });
+    /* modalLayerPopup */
+
+});
